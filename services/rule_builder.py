7c840bb061ec39205d9079a54e848396db4deb58
Riccardo Pirini <piroheavy@gmail.com>
2026-06-29 11:54:24 +0200

    Support single spread rows in rule builder

--- services/rule_builder.py
+++ services/rule_builder.py
@@ -27,19 +27,11 @@ class RuleBuilder:
 
             upper = riga.upper()
 
-            # -----------------------------
-            # FINALITA
-            # -----------------------------
-
             if "FINALITA" in upper or "SURROGA" in upper:
 
                 finalita = riga
 
                 continue
-
-            # -----------------------------
-            # TASSI
-            # -----------------------------
 
             if upper == "FISSO":
 
@@ -63,100 +55,97 @@ class RuleBuilder:
 
                 tasso = "RATA PROTETTA"
 
-            # -----------------------------
-            # DURATA
-            # -----------------------------
-
             durata = re.search(
-
-                r"(\d+)\s*-\s*(\d+)",
-
+                r"(\d+)\s*[-–]\s*(\d+)",
                 riga
-
             )
 
             spread = re.findall(
-
                 r"\d+,\d+%",
-
                 riga
-
             )
 
-            if durata and len(spread) == 4:
+            if not durata or len(spread) == 0:
 
-                durata_min = int(
+                continue
 
-                    durata.group(1)
+            durata_min = int(
+                durata.group(1)
+            )
 
-                )
+            durata_max = int(
+                durata.group(2)
+            )
 
-                durata_max = int(
-
-                    durata.group(2)
-
-                )
+            if len(spread) == 4:
 
                 for i in range(4):
 
-                    rules.append({
+                    rules.append(
+                        self._build_rule(
+                            header_info=header_info,
+                            finalita=finalita,
+                            tasso=tasso,
+                            durata_min=durata_min,
+                            durata_max=durata_max,
+                            ltv_max=colonne_ltv[i],
+                            spread=spread[i]
+                        )
+                    )
 
-                        "id": self.rule_id,
+            elif len(spread) == 1:
 
-                        "tipo_listino":
-
-                            header_info.get(
-
-                                "tipo_listino",
-
-                                ""
-
-                            ),
-
-                        "canalizzazione_da":
-
-                            header_info.get(
-
-                                "canalizzazione_da",
-
-                                ""
-
-                            ),
-
-                        "canalizzazione_a":
-
-                            header_info.get(
-
-                                "canalizzazione_a",
-
-                                ""
-
-                            ),
-
-                        "stipula_entro":
-
-                            header_info.get(
-
-                                "stipula_entro",
-
-                                ""
-
-                            ),
-
-                        "finalita": finalita,
-
-                        "tasso": tasso,
-
-                        "durata_min": durata_min,
-
-                        "durata_max": durata_max,
-
-                        "ltv_max": colonne_ltv[i],
-
-                        "spread": spread[i]
-
-                    })
-
-                    self.rule_id += 1
+                rules.append(
+                    self._build_rule(
+                        header_info=header_info,
+                        finalita=finalita,
+                        tasso=tasso,
+                        durata_min=durata_min,
+                        durata_max=durata_max,
+                        ltv_max=80,
+                        spread=spread[0]
+                    )
+                )
 
         return rules
+
+    def _build_rule(
+        self,
+        header_info,
+        finalita,
+        tasso,
+        durata_min,
+        durata_max,
+        ltv_max,
+        spread
+    ):
+
+        rule = {
+            "id": self.rule_id,
+            "tipo_listino": header_info.get(
+                "tipo_listino",
+                ""
+            ),
+            "canalizzazione_da": header_info.get(
+                "canalizzazione_da",
+                ""
+            ),
+            "canalizzazione_a": header_info.get(
+                "canalizzazione_a",
+                ""
+            ),
+            "stipula_entro": header_info.get(
+                "stipula_entro",
+                ""
+            ),
+            "finalita": finalita,
+            "tasso": tasso,
+            "durata_min": durata_min,
+            "durata_max": durata_max,
+            "ltv_max": ltv_max,
+            "spread": spread
+        }
+
+        self.rule_id += 1
+
+        return rule
