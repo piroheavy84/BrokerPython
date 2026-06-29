bc70cb3f7efe5decbefb8d7f4f7f36786d180add
Riccardo Pirini <piroheavy@gmail.com>
2026-06-29 15:34:51 +0200

    Add rogito date to mortgage model

--- models/mortgage.py
+++ models/mortgage.py
@@ -1,17 +1,12 @@
 class Mortgage:
 
     def __init__(
-
         self,
-
         importo,
-
         durata,
-
         finalita,
-
-        tasso
-
+        tasso,
+        data_rogito=None
     ):
 
         self.importo = importo
@@ -21,3 +16,5 @@ class Mortgage:
         self.finalita = finalita
 
         self.tasso = tasso
+
+        self.data_rogito = data_rogito
